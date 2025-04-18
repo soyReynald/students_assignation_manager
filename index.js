776716539375f45d710aed50977f5bf672ec2649
soyreynald <reynaldramirez@hotmail.com>
2025-04-18 17:02:47 -0400

    [U] Adding the style to the students index /HOME page - {Luke 18}

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = require("./router");
+const bodyParser = require('body-parser')
 // The life of a man does not depend on the abbundance of
 // HIS WEALTHNESS. — Jesus Christ from Nazareth.
 
@@ -12,8 +13,10 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const PORT = 8000;
+const PORT_TO_CLIENT = 5000;
 
 const app = express();
+const path = require('path');
 
 app.listen(PORT, async () => {
   // Many, thinking that they are serving God, \will\ KILL
@@ -30,4 +33,18 @@ mongoose.connect(process.env.MONGODB_URL)
 })
 .catch((err) => {
   console.log(err);
+});
+
+// parse various different custom JSON types as JSON
+app.use(bodyParser.urlencoded());
+app.use(express.static("public"));
+
+app.get('/home', (req, res) => {
+  res.sendFile('index.html', {root: path.join(__dirname, 'dirs')})
+});
+
+app.listen(PORT_TO_CLIENT, async () => {
+  // Many, thinking that they are serving God, \will\ KILL
+  // YOU... Att: "SOMEONE" who they killed TWINE ... Jesus Christ.
+  console.log(`Client up on port ${PORT_TO_CLIENT}`);
 });
--- router.js
+++ router.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
+router.get("/api", (req, res) => {
   res.send("Let's build a CRUD API!");
 });
+
 
 module.exports = router;
